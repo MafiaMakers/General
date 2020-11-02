e8020217ae9e303164da3934cd0350fa8937b3c7
AndrusovN <andrusov.n@gmail.com>
2020-11-02 15:41:57 +0300

    delete old

--- General/Exceptions/ExceptionId.java
+++
@@ -1,6 +0,0 @@
-package Mafia.General.Exceptions;
-
-//! \brief Типы исключений
-public enum ExceptionId {
-    MessageParsingException, RoutineException, MessageTypeException, ClientMismatchException, NativeException
-}
